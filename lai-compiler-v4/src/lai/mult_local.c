be0e9e1e05f7cbbf2bf64e2eeb3e75372a16ef0a
VkRobe <s-stevensr@protonmail.ch>
2019-01-21 16:27:59 -0800

    some more tests

---
+++ lai-compiler-v4/src/lai/mult_local.c
@@ -0,0 +1,16 @@
+multiplyNumbers(a : int, b : int) : int {
+	
+	return doTask()
+	
+	addNumbers(num1 : int, num2 : int) : int {
+		return num1 + num2
+	}
+	
+	doTask() : int {
+		output : int
+		for(i -> a) {
+			output = addNumbers(output, b)
+		}
+		return output
+	}
+}
